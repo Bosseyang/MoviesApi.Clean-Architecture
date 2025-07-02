9a4a72248923a235cb7ffb7d2f700c51c7de37e7
Bosseyang <bosse_yang@hotmail.com>
2025-07-02 17:11:33 +0200

    Added SeedDataExtension

---
+++ MovieApi/Extensions/SeedDataExtension.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Data;
+using System.Diagnostics;
+
+namespace MovieApi.Extensions;
+
+public static class SeedDataExtension
+{
+    public static async Task SeedDataAsync(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        var serviceProvider = scope.ServiceProvider;
+        var context = serviceProvider.GetRequiredService<MovieContext>();
+
+        //Auto update and migrate
+        await context.Database.EnsureDeletedAsync();
+        await context.Database.MigrateAsync();
+
+        try
+        {
+            await SeedData.InitAsync(context);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            throw;
+        }
+    }
+}
