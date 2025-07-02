c496846a95f81d353a55977381e101f5e6e86083
Bosseyang <bosse_yang@hotmail.com>
2025-07-02 09:04:26 +0200

    Added MovieDetails

--- MovieApi/Models/Entities/Movie.cs
+++ MovieApi/Models/Entities/Movie.cs
@@ -6,7 +6,7 @@ public class Movie
 
     public string Title { get; set; } = null!;
     public int Year { get; set; }
-
+    //TODO: Normalize later?
     public string Genre { get; set; } = null!;
     public int Duration { get; set; }
 
