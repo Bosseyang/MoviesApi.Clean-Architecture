ab610aebe3e0869c6676457027ac59070242ddc5
Bosseyang <bosse_yang@hotmail.com>
2025-07-03 19:28:27 +0200

    Added MovieDetailCreateDto to handle Post more nicely

---
+++ MovieApi/Models/DTOs/MovieDetailCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApi.Models.DTOs;
+
+public class MovieDetailCreateDto
+{
+    [Required]
+    public string Synopsis { get; set; } = string.Empty;
+
+    [Required]
+    public string Language { get; set; } = string.Empty;
+
+    [Range(0, int.MaxValue)]
+    public int Budget { get; set; }
+}
