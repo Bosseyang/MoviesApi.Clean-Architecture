0feec68a7116e000178c44fec0556292a0fe3462
Bosseyang <bosse_yang@hotmail.com>
2025-07-03 17:52:13 +0200

    Added ActorsController, needs updating Added migrations

---
+++ MovieApi/Controllers/ActorsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieApi.Data;
+using MovieApi.Models.DTOs;
+using MovieApi.Models.Entities;
+
+namespace MovieApi.Controllers
+{
+    [ApiController]
+    [Route("api/actors")]
+    //[Produces("application/json")]
+    //TODO: Add Swashbuckle.AspNetCore.Annotations 
+    public class ActorsController : ControllerBase
+    {
+        private readonly MovieContext _context;
+
+        public ActorsController(MovieContext context)
+        {
+            _context = context;
+        }
+
+        // POST: /api/movies/{movieId}/actors/{actorId}
+        // TODO: (lägg till aktör till film med roll)
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPost]
+        //public async Task<ActionResult<Actor>> PostActor(Actor actor)
+        //{
+        //    _context.Actors.Add(actor);
+        //    await _context.SaveChangesAsync();
+
+        //    return CreatedAtAction("GetMovie", new { id = actor.Id }, actor);
+        //}
+
+
+        private bool ActorExists(int id)
+        {
+            return _context.Actors.Any(e => e.Id == id);
+        }
+    }
+}
+
