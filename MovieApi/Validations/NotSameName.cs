e7198f5d1cc7f3d135aa21fe8164ac3c01bdb8ce
Bosseyang <bosse_yang@hotmail.com>
2025-07-04 17:14:04 +0200

    Validations added, Needs to Implement and update appropriately later.

---
+++ MovieApi/Validations/NotSameName.cs
@@ -0,0 +1,26 @@
+﻿using MovieApi.Data;
+using MovieApi.Models.DTOs;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieApi.Validations;
+
+public class NotSameName : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        //Example
+        //var context = validationContext.GetRequiredService<MovieContext>();
+
+        if(value is string input)
+        {
+            if(validationContext.ObjectInstance is MovieCreateDto dto)
+            {
+                return dto.Title.Trim().Equals(input.Trim(), StringComparison.OrdinalIgnoreCase) ?
+                    new ValidationResult("Title cannot be same") :
+                    ValidationResult.Success;
+            }
+        }
+
+        return new ValidationResult("Error");
+    }
+}
