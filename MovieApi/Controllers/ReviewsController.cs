e14d4432c46dcfaba8d71f04e983d3092abae7e5
Bosseyang <bosse_yang@hotmail.com>
2025-07-04 18:34:47 +0200

    Added and implemented ReviewsController. May add more Routes if time allows.

---
+++ MovieApi/Controllers/ReviewsController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieApi.Data;
+using MovieApi.Models.DTOs;
+
+namespace MovieApi.Controllers;
+
+[ApiController]
+[Route("api/movies")]
+//[Produces("application/json")]
+//TODO: Add Swashbuckle.AspNetCore.Annotations 
+public class ReviewsController : ControllerBase
+{
+    private readonly MovieContext _context;
+    private readonly IMapper _mapper;
+
+    public ReviewsController(MovieContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    //GET: /api/movies/{movieId}/reviews
+    [HttpGet("{movieId}/reviews")]
+    public async Task<ActionResult<IEnumerable<ReviewDto>>> GetReviews(int movieId)
+    {
+        var dto = await _mapper
+            .ProjectTo<ReviewDto>(_context.Reviews.Where(r => r.MovieId == movieId))
+            .ToListAsync();
+
+        if (dto == null)
+            return NotFound();
+
+        return Ok(dto);
+    }
+}
+
+
