e49d71bd72317d297d6f7081834f0e32658a6c4d
Bosseyang <bosse_yang@hotmail.com>
2025-07-07 16:31:03 +0200

    Added MovieActors and its relations

---
+++ MovieApi/Models/Entities/MovieActor.cs
@@ -0,0 +1,15 @@
+﻿namespace MovieApi.Models.Entities;
+
+public class MovieActor
+{
+    public int MovieId { get; set; }
+
+    public int ActorId { get; set; }
+
+    public string Role { get; set; } = null!;
+
+    //Navigation Property
+    public Movie Movie { get; set; } = null!;
+    public Actor Actor { get; set; } = null!;
+
+}
