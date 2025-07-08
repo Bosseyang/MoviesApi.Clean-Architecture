7bc373f64f7f1eeae75e1d22af6dfb35b11e2ce9
Bosseyang <bosse_yang@hotmail.com>
2025-07-08 16:46:48 +0200

    Updated migrations

---
+++ MovieApi/Migrations/20250708143737_MovieActor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class MovieActor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Role",
+                table: "MovieActor",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Role",
+                table: "MovieActor",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+        }
+    }
+}
