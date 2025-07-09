f1bc8abe9485a44439fcd5549b28abded3aa888d
Bosseyang <bosse_yang@hotmail.com>
2025-07-09 16:43:55 +0200

    Added ReportsController, no methods added yet

---
+++ MovieApi/Controllers/ReportsController.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieApi.Data;
+using MovieApi.Models.DTOs;
+
+namespace MovieApi.Controllers;
+
+[ApiController]
+[Route("api/reports")]
+public class ReportsController : ControllerBase
+{
+    private readonly MovieContext _context;
+    private readonly IMapper _mapper;
+
+    public ReportsController(MovieContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+}
