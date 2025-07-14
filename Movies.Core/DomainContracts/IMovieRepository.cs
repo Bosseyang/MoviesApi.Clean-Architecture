0f087c00b3f56677634a1383bc04048e9ea09911
Bosseyang <bosse_yang@hotmail.com>
2025-07-14 17:34:29 +0200

    Get is implemented, the other methods to be updated and implemented too.

--- Movies.Core/DomainContracts/IMovieRepository.cs
+++ Movies.Core/DomainContracts/IMovieRepository.cs
@@ -1,4 +1,5 @@
-﻿using Movies.Core.Entities;
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,8 +11,9 @@ namespace Movies.Core.DomainContracts;
 public interface IMovieRepository
 {
     Task<IEnumerable<Movie>> GetAllAsync();
+    //Task<IEnumerable<Movie>> GetAllAsync([FromQuery] bool withactors = false);
     Task<Movie?> GetAsync(int id);
-    Task<bool> AnyAsync(int id);
+    //Task<bool> AnyAsync(int id);
     void Add(Movie movie);
     void Update(Movie movie);
     void Remove(Movie movie);
