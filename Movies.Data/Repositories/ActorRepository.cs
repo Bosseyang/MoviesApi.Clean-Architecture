859f0ab66b306e09e71e8f472045a9e5d865e9ff
Bosseyang <bosse_yang@hotmail.com>
2025-07-14 17:33:17 +0200

    Added some more repositories, to be updated

---
+++ Movies.Data/Repositories/ActorRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Data.Repositories;
+
+public class ActorRepository
+{
+    private readonly MovieContext _context;
+    public ActorRepository(MovieContext context) => _context = context;
+    public async Task AddActorToMovieAsync(int movieId, int actorId, string role)
+    {
+        var movieActor = new MovieActor
+        {
+            MovieId = movieId,
+            ActorId = actorId,
+            Role = role
+        };
+
+        _context.MovieActors.Add(movieActor);
+        await _context.SaveChangesAsync();
+    }
+
+    public void AddActorToMovieWithRoleAsync(int movieId)
+    {
+
+    }
+}
--- Movies.Data/Repositories/MovieRepository.cs
+++ Movies.Data/Repositories/MovieRepository.cs
@@ -1,30 +1,33 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Movies.Core.DomainContracts;
+using Movies.Core.DTOs;
 using Movies.Core.Entities;
 
 namespace Movies.Data.Repositories;
 
-internal class MovieRepository : IMovieRepository
+public class MovieRepository : IMovieRepository
 {
 
     private readonly MovieContext _context;
     public MovieRepository(MovieContext context) => _context = context;
 
-
-
-
-
-    public Task<bool> AnyAsync(int id)
-    {
-        throw new NotImplementedException();
-    }
-
     public async Task<IEnumerable<Movie>> GetAllAsync() => await _context.Movies.ToListAsync();
+    //public async Task<IEnumerable<Movie>> GetAllAsync([FromQuery] bool withactors = false)
+    //{
+
+    //    var query = _context.Movies.AsQueryable();
+
+    //    if (withactors) query = query.Include(m => m.MovieActors).ThenInclude(ma => ma.Actor);
+
+    //    return await query.ToListAsync();
+    //}
 
     public async Task<Movie?> GetAsync(int id) => await _context.Movies
         .Include(m => m.MovieDetails)
         .Include(m => m.Reviews)
-        .Include(m => m.MovieActors).ThenInclude(ma => ma.Actor)
+        .Include(m => m.MovieActors)//.ThenInclude(ma => ma.Actor)
         .FirstOrDefaultAsync(m => m.Id == id);
 
     public void Add(Movie movie) => _context.Movies.Add(movie);
