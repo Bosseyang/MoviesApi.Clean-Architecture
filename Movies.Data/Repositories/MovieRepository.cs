ce14314dc68bdbf024efb5d465b5fbd0fc64a375
Bosseyang <bosse_yang@hotmail.com>
2025-07-14 16:22:54 +0200

    Added Movierepository, not finished yet, to be updated.

---
+++ Movies.Data/Repositories/MovieRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.Core.DomainContracts;
+using Movies.Core.Entities;
+
+namespace Movies.Data.Repositories;
+
+internal class MovieRepository : IMovieRepository
+{
+
+    private readonly MovieContext _context;
+    public MovieRepository(MovieContext context) => _context = context;
+
+
+
+
+
+    public Task<bool> AnyAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Movie>> GetAllAsync() => await _context.Movies.ToListAsync();
+
+    public async Task<Movie?> GetAsync(int id) => await _context.Movies
+        .Include(m => m.MovieDetails)
+        .Include(m => m.Reviews)
+        .Include(m => m.MovieActors).ThenInclude(ma => ma.Actor)
+        .FirstOrDefaultAsync(m => m.Id == id);
+
+    public void Add(Movie movie) => _context.Movies.Add(movie);
+    public void Remove(Movie movie) => _context.Movies.Remove(movie);
+    public void Update(Movie movie) => _context.Movies.Update(movie);
+}
