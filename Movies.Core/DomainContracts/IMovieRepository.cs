d6ac18bc5941bdcf42957d2d50dc0efdb2bbaee8
Bosseyang <bosse_yang@hotmail.com>
2025-07-14 16:21:53 +0200

    Added Interface for MovieRepository, to be updated

---
+++ Movies.Core/DomainContracts/IMovieRepository.cs
@@ -0,0 +1,18 @@
+﻿using Movies.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Core.DomainContracts;
+
+public interface IMovieRepository
+{
+    Task<IEnumerable<Movie>> GetAllAsync();
+    Task<Movie?> GetAsync(int id);
+    Task<bool> AnyAsync(int id);
+    void Add(Movie movie);
+    void Update(Movie movie);
+    void Remove(Movie movie);
+}
