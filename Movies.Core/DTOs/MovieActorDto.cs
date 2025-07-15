837a3b27629acfe1e5ea5d079069f890f8c81958
Bosseyang <bosse_yang@hotmail.com>
2025-07-15 16:43:48 +0200

    Added to implement GET /api/movies/{movieId}/actors

---
+++ Movies.Core/DTOs/MovieActorDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Core.DTOs;
+
+public class MovieActorDto
+{
+    public int ActorId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int BirthYear { get; set; }
+    public string Role { get; set; } = string.Empty;
+}
