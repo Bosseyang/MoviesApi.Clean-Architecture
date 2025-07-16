523d737d5a1d688c0ebd3ac1ccfca156b092901a
Bosseyang <bosse_yang@hotmail.com>
2025-07-16 17:11:41 +0200

    Refactored to use UnitOfWork instead of Repository.

--- MovieApi/Controllers/ActorsController.cs
+++ MovieApi/Controllers/ActorsController.cs
@@ -1,10 +1,8 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Movies.Core.DomainContracts;
 using Movies.Core.DTOs;
 using Movies.Core.Entities;
-using Movies.Data;
-using Movies.Data.Repositories;
 
 
 // TODO: Might want to move the logic etc out of there eventually.
@@ -14,13 +12,13 @@ namespace MovieApi.Controllers
     [Route("api/movies")]
     public class ActorsController : ControllerBase
     {
-        private readonly IActorRepository _repository;
         private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public ActorsController(IActorRepository repository, IMapper mapper)
+        public ActorsController(IMapper mapper, IUnitOfWork unitOfWork)
         {
-            _repository = repository;
             _mapper = mapper;
+            _unitOfWork = unitOfWork;
         }
 
 
@@ -28,10 +26,10 @@ namespace MovieApi.Controllers
         [HttpGet("{movieId}/actors")]
         public async Task<ActionResult<IEnumerable<MovieActorDto>>> GetActorsByMovie(int movieId)
         {
-            if (!await _repository.MovieExistsAsync(movieId))
+            if (!await _unitOfWork.Actors.MovieExistsAsync(movieId))
                 return NotFound($"Movie with Id: {movieId} not found");
 
-            var dto = _mapper.Map<IEnumerable<MovieActorDto>>(await _repository.GetActorsByMovieAsync(movieId));
+            var dto = _mapper.Map<IEnumerable<MovieActorDto>>(await _unitOfWork.Actors.GetActorsByMovieAsync(movieId));
 
             return Ok(dto);
         }
@@ -40,39 +38,39 @@ namespace MovieApi.Controllers
         [HttpPost("{movieId}/actors/{actorId}")]
         public async Task<ActionResult<MovieDto>> AddActorToMovie(int movieId, int actorId)
         {
-            if (!await _repository.MovieExistsAsync(movieId))
+            if (!await _unitOfWork.Actors.MovieExistsAsync(movieId))
                 return NotFound($"Movie with Id: {movieId} not found");
 
-            if (!await _repository.ActorExistsAsync(actorId))
+            if (!await _unitOfWork.Actors.ActorExistsAsync(actorId))
                 return NotFound($"Actor with Id: {actorId} not found");
 
-            if (await _repository.ActorAlreadyInMovieAsync(movieId, actorId))
+            if (await _unitOfWork.Actors.ActorAlreadyInMovieAsync(movieId, actorId))
                 return BadRequest("Actor already added.");
 
-            var movie = await _repository.GetMovieWithActorsAsync(movieId);
+            var movie = await _unitOfWork.Actors.GetMovieWithActorsAsync(movieId);
             if (movie == null) return NotFound();
 
-            await _repository.AddActorToMovieAsync(movie, actorId);
+            await _unitOfWork.Actors.AddActorToMovieAsync(movie, actorId);
 
             var movieDto = _mapper.Map<MovieDto>(movie);
             return CreatedAtAction("GetMovie", "Movies", new { id = movieDto.Id }, movieDto);
         }
 
-// POST /api/movies/{movieId}/actors
+        // POST /api/movies/{movieId}/actors
         [HttpPost("{movieId}/actors")]
         public async Task<ActionResult> AddActorToMovieWithRole(int movieId, MovieActorCreateDto dto)
         {
-            if (!await _repository.MovieExistsAsync(movieId))
+            if (!await _unitOfWork.Actors.MovieExistsAsync(movieId))
                 return NotFound($"Movie with Id: {movieId} not found");
 
-            if (!await _repository.ActorExistsAsync(dto.ActorId))
+            if (!await _unitOfWork.Actors.ActorExistsAsync(dto.ActorId))
                 return NotFound($"Actor with Id: {dto.ActorId} not found");
 
-            if (await _repository.ActorAlreadyInMovieAsync(movieId, dto.ActorId))
+            if (await _unitOfWork.Actors.ActorAlreadyInMovieAsync(movieId, dto.ActorId))
                 return BadRequest("Actor already added to this movie.");
 
             var movieActor = _mapper.Map<MovieActor>(dto);
-            await _repository.AddActorToMovieWithRoleAsync(movieId, movieActor);
+            await _unitOfWork.Actors.AddActorToMovieWithRoleAsync(movieId, movieActor);
 
             return NoContent();
         }
