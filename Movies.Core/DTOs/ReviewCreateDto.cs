5a905ccbebdc35e525950f7f21c3a6d4b37f2ec2
Bosseyang <bosse_yang@hotmail.com>
2025-07-16 17:32:45 +0200

    Created to implement POST: /api/movies/{movieId}/reviews

---
+++ Movies.Core/DTOs/ReviewCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movies.Core.DTOs;
+
+public class ReviewCreateDto
+{
+    [Required]
+    public string ReviewerName { get; set; } = string.Empty;
+    [Required]
+    public string Comment { get; set; } = string.Empty;
+    [Required]
+    [Range(1, 5)]
+    public int Rating { get; set; }
+
+}
