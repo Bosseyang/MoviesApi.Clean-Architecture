0d46981840748310c0e1ac4d3c6e9cbf42b0c134
Bosseyang <bosse_yang@hotmail.com>
2025-07-17 20:17:19 +0200

    Added paging

---
+++ Movies.Core/DTOs/PagedResults.cs
@@ -0,0 +1,17 @@
+﻿namespace Movies.Core.DTOs
+{
+    public class PagedResult<T>
+    {
+        public IEnumerable<T> Data { get; set; } = new List<T>();
+
+        public MetaData Meta { get; set; } = new();
+    }
+
+    public class MetaData
+    {
+        public int TotalItems { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+    }
+}
