a64db8ce6c016d36ee141ec43dc259aab3f898eb
Bosseyang <bosse_yang@hotmail.com>
2025-07-17 14:53:05 +0200

    Added and preparing to refactor common methods used in repositories into BaseRepository.

---
+++ Movies.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Data.Repositories;
+
+//public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class //Do Entitybase
+//{
+//    // protected ApplicationDbContext Context { get; }
+//    protected DbSet<T> DbSet { get; }
+
+//    public RepositoryBase(MovieContext context)
+//    {
+//        // Context = context;
+//        DbSet = context.Set<T>();
+//    }
+
+//    public IQueryable<T> FindAll(bool trackChanges = false) =>
+//        !trackChanges ? DbSet.AsNoTracking() :
+//                        DbSet;
+
+//    public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges = false) =>
+//        !trackChanges ? DbSet.Where(expression).AsNoTracking() :
+//                        DbSet.Where(expression);
+
+//    public void Create(T entity) => DbSet.Add(entity);
+
+//    public void Update(T entity) => DbSet.Update(entity);
+
+//    public void Delete(T entity) => DbSet.Remove(entity);
+//}
