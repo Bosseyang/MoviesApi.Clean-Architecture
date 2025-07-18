4015c76ef083b28e43f8cdaa74a208f0009a0ba8
Bosseyang <bosse_yang@hotmail.com>
2025-07-18 14:13:32 +0200

    Added ProblemDetailsException to handle more customized exceptions

---
+++ Movies.Core/Exceptions/ProblemDetailsException.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+public class ProblemDetailsException : Exception
+{
+    public ProblemDetails ProblemDetails { get; }
+
+    public ProblemDetailsException(string title, int statusCode, string detail)
+    {
+        ProblemDetails = new ProblemDetails
+        {
+            Title = title,
+            Detail = detail,
+            Status = statusCode,
+        };
+    }
+}
